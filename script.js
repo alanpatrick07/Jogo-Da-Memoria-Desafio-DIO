d191bf8db6fd427e9902b2f22e1c4a50f375fd9a
alanpatrick07 <alanpatrick@mail.ru>
2022-07-10 01:13:05 -0300

    Jogo-Da-Memoria-Desafio-DIO

---
+++ script.js
@@ -0,0 +1,87 @@
+const audioError = new Audio('assets/audio/error.mp3');
+const audioSuccess = new Audio('assets/audio/success.mp3');
+
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+//função para virar carta
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+//função que checa se as cartas são iguais
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        setTimeout(() => {
+            audioSuccess.play();
+        }, 750);
+        const flipped = document.querySelectorAll('.flip');
+        if (flipped.length === 12) gameComplete();
+        return;
+    }
+
+    unflipCards();
+}
+
+//função que desabilita as cartas
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//funcão que desvira as cartas
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        audioError.play();
+    }, 750);
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+//função que reseta o tabuleiro
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
+//função para game concluído
+function gameComplete() {
+    setTimeout(() => {
+        alert("PARABÉNS VOCÊ COMPLETOU O JOGO!");
+    }, 750);
+}
+
+//adiciona evento de clique na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
